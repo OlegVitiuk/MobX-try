e40221e810472d49046a5fa2318a16aeee7f9595
vitiukoleh@gmail.com <vitiukoleh@gmail.com>
2019-04-20 19:13:15 +0300

    basic setup2

---
+++ src/stores/BirdStore.js
@@ -0,0 +1,12 @@
+import { observable, action, computed } from 'mobx';
+
+class BirdStore {
+  @observable birds = [];
+
+  @action addBird = bird => {
+    this.birds.push(bird);
+  };
+}
+
+const store = new BirdStore();
+export default store;
